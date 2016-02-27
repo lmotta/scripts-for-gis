271f9437abbc24a20960877691d420b162bb04a1
lmotta <motta.luiz@gmail.com>
2016-02-27 08:23:29 -0300

    Create water_src_sqlite.sql

---
+++ water_src_sqlite.sql
@@ -0,0 +1,15 @@
+SELECT b.id, Centroid( b.geometry ) as geometry
+FROM
+(
+  SELECT  id, Buffer( StartPoint( geometry ),  0.001 ) as geometry
+  FROM HIDRO_100000_DOCE
+  --
+  UNION
+  --
+  SELECT  id, Buffer( EndPoint( geometry ),  0.001 ) as geometry
+  FROM HIDRO_100000_DOCE"
+)b
+INNER JOIN HIDRO_100000_DOCE l
+ON MbrIntersects( b.geometry, l.geometry ) AND Intersects( b.geometry, l.geometry)
+GROUP BY b.id, b.geometry
+HAVING Count(1) = 1;
