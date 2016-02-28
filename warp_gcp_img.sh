5ad6c388069f8e33c3d8dd2840a686747eb06c3b
lmotta <motta.luiz@gmail.com>
2016-02-28 18:44:19 -0300

    Update warp_gcp_img.sh

--- warp_gcp_img.sh
+++ warp_gcp_img.sh
@@ -6,10 +6,34 @@
 msg_error(){
   local name_script=$(basename $0)
   echo "Total of arguments is $runargs"  >&2
-  echo "Usage: $name_script <shp_original> <gcp_bb>" >&2
-  echo "<shp_original> is the file of original shapefile to warp" >&2
-  echo "<gcp> is the file of GCP(Georef)" >&2
+  echo "Usage: $name_script <image_original> <gcp_bb>" >&2
+  echo "<image_original> is the file of original image to warp" >&2
+  echo "<gcp> is the file of GCP(RAW)" >&2
   exit 1
+}
+#
+calc_params_image(){
+  # Spatial Reference System - By file
+  srs_def=$(dirname $r_img)"/"$(basename  ${r_img%.*}".wkt")
+  echo $(gdalsrsinfo -o wkt $r_img) > $srs_def
+  #
+  local tmpinfo=$(mktemp)
+  gdalinfo $r_img > $tmpinfo
+  # 
+  local origin=$(cat $tmpinfo | grep "Upper Left" | cut -d'(' -f2 | cut -d')' -f1)
+  ulx=$(echo $origin | cut -d',' -f1)
+  uly=$(echo $origin | cut -d',' -f2)
+  #  
+  origin=$(cat $tmpinfo | grep "Lower Right" | cut -d'(' -f2 | cut -d')' -f1)
+  lrx=$(echo $origin | cut -d',' -f1)
+  lry=$(echo $origin | cut -d',' -f2)
+  #
+  # Resolution
+  local res_xy=$(cat $tmpinfo | grep "Pixel Size" | cut -d'(' -f2 | cut -d')' -f1)
+  xres=$(echo $res_xy | cut -d',' -f1)
+  yres=$(echo $res_xy | cut -d',' -f2)
+  #
+  rm $tmpinfo
 }
 #
 runargs=$#
@@ -20,10 +44,10 @@ if [ $runargs -ne $totalargs ] ; then
   exit 1
 fi
 #
-r_shp=$1
+r_img=$1
 r_gcp=$2
 #
-if [ ! -f "$r_shp" ] ; then
+if [ ! -f "$r_img" ] ; then
   printf "Not found file '%s'\n" $r_img
   msg_error
   exit 1
@@ -34,12 +58,22 @@ if [ ! -f "$r_gcp" ] ; then
   exit 1
 fi
 #
-w_shp=$(dirname $r_shp)"/"$(basename ${r_shp%.*}"_warp.shp")
-#
-gcp=$(cat $r_gcp)
+w_img=$(dirname $r_img)"/"$(basename ${r_img%.*}"_warp.tif")
 #
 printf "Warpping..."
 #
-ogr2ogr -overwrite $gcp -order 1 $w_shp $r_shp
+calc_params_image
+gcp=$(cat $r_gcp)
+# Cleanup Image and set GCP and Warp
+gdal_edit.py -unsetgt $gcp $r_img
+# Warp resample (-r ): cubic(useb byt CR) near(classification)
+#gdalwarp -t_srs $srs_def -order 1 -tr $xres $yres -te $ulx $lry $lrx $uly -r cubic -multi -q -setci -overwrite  $r_img $w_img
+gdalwarp -t_srs $srs_def -order 1 -ts 5000 5000 -te $ulx $lry $lrx $uly -r near -multi -q -setci -overwrite  $r_img $w_img
+# Set original SRS
+rm $r_img".aux.xml"
+gdal_edit.py -unsetgt $r_img
+gdal_edit.py -a_srs $srs_def -a_ullr $ulx $uly $lrx $lry $r_img
+gdal_edit.py -tr $xres $yres $r_img
+rm $srs_def
 #
-printf "\r%-100s\n" "'$w_shp' created!"
+printf "\r%-100s\n" "'$w_img' created!"
