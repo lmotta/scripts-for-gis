e281de9cc6319031eb361fc60fffa37a3dd96e0e
lmotta <motta.luiz@gmail.com>
2017-02-21 14:47:32 -0300

    Update rename_id_shps-in_dir.py

--- rename_id_shps-in_dir.py
+++ rename_id_shps-in_dir.py
@@ -25,6 +25,18 @@ import os, sys, fnmatch, argparse
 
 
 def run(rootDir):
+  def renameFiles(root, filenames, idDir, idFile):
+    vwildcard3 = "{0}.*".format( os.path.splitext( filename )[0] )
+    filter2 = fnmatch.filter( filenames, vwildcard3 ) # names start with idFile         
+    for name in filter2:
+      newname = name.replace(idFile, idDir)
+      vold = os.path.join(root, name )
+      vnew = os.path.join(root, newname )
+      try:
+        os.rename( vold, vnew)
+      except OSError as e:
+        sys.stdout.write(" - Error '{0}'".format( name ) )
+    
   if not os.path.isdir( rootDir ):
     msg = "'{0}' is not valid directory\n.".format( rootDir )
     sys.stderr.write( msg )
@@ -36,27 +48,21 @@ def run(rootDir):
   totalRename = 0
   for root, dirnames, filenames in os.walk( rootDir ):
     l_filter1 = fnmatch.filter( filenames, vwildcard1 ) + fnmatch.filter( filenames, vwildcard2 )
+    idDir = root.split(os.path.sep)[-1].split('_')[0]
     for filename in l_filter1:
-      idDir = root.split(os.path.sep)[-1].split('_')[0]
       idFile = filename.split('_')[0]
-      if not idDir == idFile:
-        totalRename += 1
-        msg = "\rRename '{0}' with id '{1}'".format( filename, idDir )
-        sys.stdout.write( msg )
-        vwildcard3 = "{0}.*".format( os.path.splitext( filename )[0] )
-        filter2 = fnmatch.filter( filenames, vwildcard3 ) # names of shapefiles files        for name in         
-        for name in filter2:
-          newname = "{0}_{1}".format( idDir, name.split('_')[1] )
-          if name == newname:
-            continue
-          vold = os.path.join(root, name )
-          vnew = os.path.join(root, newname )
-          os.rename( vold, vnew)
+      if idDir == idFile:
+        continue
+      totalRename += 1
+      sys.stdout.write( "\r{0:100s}".format( '' ) ) # Clean
+      msg = "\rRename '{0}' with id '{1}'".format( filename, idDir )
+      sys.stdout.write( msg )
+      renameFiles( root, filenames, idDir, idFile )
   if totalRename == 0:
-    sys.stdout.write( "Not need rename shapefiles\n" )
+    msg = "Not need rename shapefiles"
   else:
     msg = "Finished (total = {0})".format(totalRename)
-    sys.stdout.write( "\r{0:100s}\n".format( msg ) )
+  sys.stdout.write( "\r{0:100s}\n".format( msg ) )
   return 0
 
 def main():
